174d543aa1bc7c253179b1fd0ffc4a1d384f5251
Vincent Ting <homerdd@gmail.com>
2015-03-12 21:23:57 +0800

    add semantic and gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require("gulp-rename");
+var prettify = require('gulp-jsbeautifier');
+
+gulp.task('default', function () {
+    console.log("Hello Blog");
+});
+
+gulp.task('build-less', function () {
+    gulp.src('./resources/assets/css/*/*.less')
+        .pipe(less({compress: true}))
+        .on('error', function (e) {
+            console.log(e);
+        })
+        .pipe(prettify({indentSize: 2}))
+        .pipe(rename(function (path) {
+            path.dirname = '';
+        }))
+        .pipe(gulp.dest('./resources/assets/css/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch([
+        './resources/assets/css/**/*.less',
+        './resources/assets/css/**/*.variables',
+        './resources/assets/css/**/*.overrides'
+    ], ['build-less']);
+});
